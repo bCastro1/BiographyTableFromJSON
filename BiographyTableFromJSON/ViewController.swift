6f36bb3b5171d0169aca84b2762cb4df8a11b707
Brendan Castro <brendan064@gmail.com>
2017-03-13 10:11:29 -0700

    Initial Commit

---
+++ BiographyTableFromJSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BiographyTableFromJSON
+//
+//  Created by Brendan Castro on 3/13/17.
+//  Copyright © 2017 Brendan Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
